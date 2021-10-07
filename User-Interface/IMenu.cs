a213d4059ab81f8d9311c4d8a422d4902e949a9c
Colin Mudie <csmudie1@gmail.com>
2021-10-07 11:26:20 -0400

    added Interface IMenu

---
+++ User-Interface/IMenu.cs
@@ -0,0 +1,28 @@
+namespace User_Interface
+{
+    public enum MenuType
+    {
+        AddCustomer,
+        SearchForCustomer,
+        ViewStoreFrontInventory,
+        PlaceOrder,
+        ViewOrderHistory,
+        ReplenishInventory,
+        Exit
+    }
+    public interface IMenu
+    {
+        /// <summary>
+        /// Will display the menu of the current menu class 
+        /// and the choices that will go along with them.
+        /// </summary>
+        void Menu();
+
+        /// <summary>
+        /// Will Record the users input choice to the change the menu
+        /// based on the end-user's choice
+        /// </summary>
+        /// <returns> This method will return a menu that the user will go to next. </returns>
+        MenuType UserChoice();
+    }
+}
